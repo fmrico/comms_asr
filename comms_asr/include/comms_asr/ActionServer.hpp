1955b31cb0d39c47a5f4ebfeee78550ea63c0bcc
Francisco Martín Rico <fmrico@gmail.com>
2023-04-10 19:14:00 +0200

    Initial version
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

---
+++ comms_asr/include/comms_asr/ActionServer.hpp
@@ -0,0 +1,52 @@
+// Copyright 2023 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COMMS_ASR__ACTION_SERVER_HPP_
+#define COMMS_ASR__ACTION_SERVER_HPP_
+
+#include "nav2_msgs/action/navigate_to_pose.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+namespace comms_asr
+{
+
+class ActionServer : public rclcpp::Node
+{
+public:
+  using NavigateToPose = nav2_msgs::action::NavigateToPose;
+  using GoalHandleNavigateToPose = rclcpp_action::ServerGoalHandle<NavigateToPose>;
+
+  ActionServer();
+  void start_server();
+
+private:
+  rclcpp_action::Server<NavigateToPose>::SharedPtr navigate_action_server_;
+  NavigateToPose::Goal current_goal_;
+
+  rclcpp_action::GoalResponse handle_goal(
+    const rclcpp_action::GoalUUID & uuid,
+    std::shared_ptr<const NavigateToPose::Goal> goal);
+  rclcpp_action::CancelResponse handle_cancel(
+    const std::shared_ptr<GoalHandleNavigateToPose> goal_handle);
+  void handle_accepted(
+    const std::shared_ptr<GoalHandleNavigateToPose> goal_handle);
+
+  void execute(const std::shared_ptr<GoalHandleNavigateToPose> goal_handle);
+};
+
+}  // namespace comms_asr
+
+#endif  // COMMS_ASR__ACTION_SERVER_HPP_
